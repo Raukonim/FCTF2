17cb2b8ef7963e42f6146c03a61bf2e5967b9fef
Raukonim <fajulanet@gmail.com>
2017-12-01 11:18:40 +0100

    Fix script

--- p4scriptMC2.sh
+++ p4scriptMC2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 gfortran -O3 -o MC2.exe MC2.f
-./MC2 "data/MC2-L008.dat"
+./MC2.exe "data/MC2-L008.dat"
 gnuplot plotscripts/P4_L8.gnu
-./MC2 "data/MC2-L016.dat"
+./MC2.exe "data/MC2-L016.dat"
 gnuplot plotscripts/P4_L16.gnu
-./MC2 "data/MC2-L032.dat"
+./MC2.exe "data/MC2-L032.dat"
 gnuplot plotscripts/P4_L32.gnu
-./MC2 "data/MC2-L064.dat"
+./MC2.exe "data/MC2-L064.dat"
 gnuplot plotscripts/P4_L64.gnu
-./MC2 "data/MC2-L128.dat"
+./MC2.exe "data/MC2-L128.dat"
 gnuplot plotscripts/P4_L128.gnu
